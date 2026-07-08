7740e00b1921cdc46f1eb478b5a4f52d9f130738
emanueltorres1993-star <emanueltorres1993@gmail.com>
2026-07-08 09:47:19 -0300

    Crear config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# ==========================
+# Emanuel Trading AI v1.0
+# Archivo de configuración
+# ==========================
+
+BOT_NAME = "Emanuel Trading AI"
+
+TIMEFRAME = 5
+
+MAX_SIGNALS_PER_DAY = 8
+
+PAIRS = [
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "AUDUSD"
+]
