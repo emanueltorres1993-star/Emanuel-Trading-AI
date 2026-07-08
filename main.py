dea65e05ddf397b18a6a5f8cb7a35b79c603a517
emanueltorres1993-star <emanueltorres1993@gmail.com>
2026-07-08 13:18:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("===================================")
-print(" Emanuel Trading AI")
-print(" Bot iniciado correctamente")
-print("===================================")
+from telegram import Bot
+import asyncio
+from config import TELEGRAM_TOKEN, CHAT_ID
+
+async def main():
+    bot = Bot(token=TELEGRAM_TOKEN)
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="🤖 Emanuel Trading AI\n\n✅ El bot está conectado correctamente."
+    )
+
+asyncio.run(main())
